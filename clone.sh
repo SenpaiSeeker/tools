7ec95febf246858fa11cd64ee02af0731d66629e
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-07-30 21:13:11 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,23 +1,78 @@
-REPO_CLONE=$1
-REPO_REMOTE=$2
-GITHUB_TOKEN=$3
-COMMIT_MESSAGE=${4:-"initial"}
+function tampilkan_menu() {
+    echo "Pilih opsi:"
+    echo "1) Clone repositori"
+    echo "2) Hosting langsung dari VPS"
+    read -p "Masukkan pilihan Anda (1 atau 2): " PILIHAN
+}
 
-git clone "$REPO_CLONE"
-cd "$(basename "$REPO_CLONE" .git)"
+function clone_repo() {
+    local REPO_CLONE=$1
+    local REPO_REMOTE=$2
+    local GITHUB_TOKEN=$3
+    local COMMIT_MESSAGE=${4:-"initial"}
 
-rm -rf .git
-git config --global user.email "support@hacker.ltd"
-git config --global user.name "hacker"
-git init
+    git clone "$REPO_CLONE"
+    cd "$(basename "$REPO_CLONE" .git)"
 
-git add .
-git commit -m "$COMMIT_MESSAGE"
-git branch -M main
+    rm -rf .git
+    git config --global user.email "support@hacker.ltd"
+    git config --global user.name "hacker"
+    git init
 
-REPO_REMOTE_CLEAN="${REPO_REMOTE#https://}"
+    git add .
+    git commit -m "$COMMIT_MESSAGE"
+    git branch -M main
 
-git remote add origin "$REPO_REMOTE"
-git remote set-url origin "https://$GITHUB_TOKEN@$REPO_REMOTE_CLEAN"
+    REPO_REMOTE_CLEAN="${REPO_REMOTE#https://}"
 
-git push -u origin main
+    git remote add origin "$REPO_REMOTE"
+    git remote set-url origin "https://$GITHUB_TOKEN@$REPO_REMOTE_CLEAN"
+
+    git push -u origin main
+}
+
+function host_repo() {
+    local REPO_REMOTE=$1
+    local GITHUB_TOKEN=$2
+    local COMMIT_MESSAGE=${3:-"initial"}
+
+    git init
+
+    git config --global user.email "support@hacker.ltd"
+    git config --global user.name "hacker"
+
+    git add .
+    git commit -m "$COMMIT_MESSAGE"
+    git branch -M main
+
+    REPO_REMOTE_CLEAN="${REPO_REMOTE#https://}"
+
+    git remote add origin "$REPO_REMOTE"
+    git remote set-url origin "https://$GITHUB_TOKEN@$REPO_REMOTE_CLEAN"
+
+    git push -u origin main
+}
+
+tampilkan_menu
+
+case $PILIHAN in
+    1)
+        read -p "Masukkan URL clone repositori: " REPO_CLONE
+        read -p "Masukkan URL repositori remote: " REPO_REMOTE
+        read -p "Masukkan token GitHub Anda: " GITHUB_TOKEN
+        read -p "Masukkan pesan commit (default: 'initial'): " COMMIT_MESSAGE
+        COMMIT_MESSAGE=${COMMIT_MESSAGE:-"initial"}
+        clone_repo "$REPO_CLONE" "$REPO_REMOTE" "$GITHUB_TOKEN" "$COMMIT_MESSAGE"
+        ;;
+    2)
+        read -p "Masukkan URL repositori remote: " REPO_REMOTE
+        read -p "Masukkan token GitHub Anda: " GITHUB_TOKEN
+        read -p "Masukkan pesan commit (default: 'initial'): " COMMIT_MESSAGE
+        COMMIT_MESSAGE=${COMMIT_MESSAGE:-"initial"}
+        host_repo "$REPO_REMOTE" "$GITHUB_TOKEN" "$COMMIT_MESSAGE"
+        ;;
+    *)
+        echo "Pilihan tidak valid. Keluar."
+        exit 1
+        ;;
+esac
