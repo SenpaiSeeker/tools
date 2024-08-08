c1b9604536e0e4f2b6d256f03d922ae722b01da6
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-08-08 14:22:46 +0700

    Update clean-cpu.sh

--- clean-cpu.sh
+++ clean-cpu.sh
@@ -1,14 +1,38 @@
-PIDS=$(ps -eo pid,pcpu,comm --sort=-pcpu | awk '$2 > 0.0 {print $1}')
+log_info() {
+    echo -e "\033[1;32mINFO:\033[0m $1"
+}
 
-for PID in $PIDS; do
-    PROC_NAME=$(ps -p $PID -o comm=)
-    CPU_USAGE=$(ps -p $PID -o %cpu=)
+log_warning() {
+    echo -e "\033[1;33mWARNING:\033[0m $1"
+}
+
+log_error() {
+    echo -e "\033[1;31mERROR:\033[0m $1"
+}
+
+log_info "Mendapatkan daftar proses yang menggunakan CPU lebih dari 10%..."
+PROCESSES=$(ps -eo pid,comm,%cpu --sort=-%cpu | awk '$3 > 10 {print $1":"$2":"$3}')
+
+if [ -z "$PROCESSES" ]; then
+    log_info "Tidak ada proses yang menggunakan CPU lebih dari 10%."
+    exit 0
+fi
+
+log_warning "Menghapus proses yang menggunakan CPU secara signifikan:"
+IFS=$'\n'
+for process in $PROCESSES; do
+    PID=$(echo $process | cut -d: -f1)
+    COMMAND=$(echo $process | cut -d: -f2)
+    CPU=$(echo $process | cut -d: -f3)
     
-    echo "Menghentikan proses: $PROC_NAME"
-    echo "PID: $PID"
-    echo "penggunaan CPU: $CPU_USAGE%"
+    log_info "Menghapus proses PID $PID (Command: $COMMAND, CPU: $CPU%)..."
+    kill -9 $PID
     
-    kill -STOP $PID
+    if [ $? -eq 0 ]; then
+        log_info "Proses PID $PID berhasil dihapus."
+    else
+        log_error "Gagal menghapus proses PID $PID."
+    fi
 done
 
-echo "Semua proses yang menggunakan CPU telah dihentikan."
+log_info "Pembersihan penggunaan CPU selesai."
