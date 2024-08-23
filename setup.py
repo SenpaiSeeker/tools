14cd5974f7bcf699f4b46a3d24d2493a3d7a1e6e
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-08-24 06:48:16 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="NS_tools",
+    version="0.1",
+    description="Library of @NorSodikin",
+    long_description="",
+    long_description_content_type="text/markdown",
+    author="NorSodikin",
+    author_email="admin@NorSodikin.com",
+    url="https://github.com/SenpaiSeeker/tools",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires="",
+)
