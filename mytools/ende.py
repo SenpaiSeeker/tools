413df18bd10273470a044418785a1748ec0affd8
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-08-24 15:18:51 +0000

    Apply automatic changes

--- mytools/ende.py
+++ mytools/ende.py
@@ -1,27 +1,28 @@
 import base64
 import hashlib
+import json
 import random
 import textwrap
-import json
+
 from cryptography import fernet
 
 
-class FARNET: 
+class FARNET:
     def __init__(self, key):
         self.key = hashlib.sha256(key.encode()).digest()
         self.cipher_suite = fernet.Fernet(base64.urlsafe_b64encode(self.key))
-                                   
+
     def en(self, data):
-        if isinstance(data, dict):  
+        if isinstance(data, dict):
             data = json.dumps(data)
-        serialized_data = textwrap.dedent(data).encode('utf-8')
+        serialized_data = textwrap.dedent(data).encode("utf-8")
         encrypted_data = self.cipher_suite.encrypt(serialized_data)
-        return encrypted_data.decode('utf-8')
+        return encrypted_data.decode("utf-8")
 
     def de(self, encrypted_data):
         try:
-            decrypted_data = self.cipher_suite.decrypt(encrypted_data.encode('utf-8'))
-            data = decrypted_data.decode('utf-8')
+            decrypted_data = self.cipher_suite.decrypt(encrypted_data.encode("utf-8"))
+            data = decrypted_data.decode("utf-8")
             try:
                 return json.loads(data)
             except json.JSONDecodeError:
@@ -32,11 +33,11 @@ class FARNET:
     def logs(self, text):
         random_color = random.choice(
             [
-                "\033[91m", 
-                "\033[92m", 
-                "\033[93m", 
+                "\033[91m",
+                "\033[92m",
+                "\033[93m",
                 "\033[94m",
-                "\033[95m", 
+                "\033[95m",
                 "\033[96m",
             ]
         )
