bc2b6abc6fed9c7985a1f56f8727e97aea81f8f3
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-08-24 05:50:40 +0000

    Apply automatic changes

--- mytools/ende.py
+++ mytools/ende.py
@@ -1,25 +1,25 @@
 import base64
 import hashlib
-import random 
+import random
 import textwrap
 
 from cryptography import fernet
 
 
-class FARNET: 
+class FARNET:
     def __init__(self, key):
         self.key = hashlib.sha256(key.encode()).digest()
         self.cipher_suite = fernet.Fernet(base64.urlsafe_b64encode(self.key))
-                                   
+
     def en(self, data):
-        serialized_data = textwrap.dedent(data).encode('utf-8')
+        serialized_data = textwrap.dedent(data).encode("utf-8")
         encrypted_data = self.cipher_suite.encrypt(serialized_data)
         return encrypted_data
 
     def de(self, encrypted_data):
         try:
             decrypted_data = self.cipher_suite.decrypt(encrypted_data)
-            data = decrypted_data.decode('utf-8')
+            data = decrypted_data.decode("utf-8")
             return data
         except fernet.InvalidToken:
             raise Exception(f"[ERROR]: KUNCI - [{self.key}] - TIDAK COCOK")
@@ -27,11 +27,11 @@ class FARNET:
     def logs(self, text):
         random_color = random.choice(
             [
-                "\033[91m", 
-                "\033[92m", 
-                "\033[93m", 
+                "\033[91m",
+                "\033[92m",
+                "\033[93m",
                 "\033[94m",
-                "\033[95m", 
+                "\033[95m",
                 "\033[96m",
             ]
         )
