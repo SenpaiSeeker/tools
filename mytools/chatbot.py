9cb86a4e13482be2f1591603c8cd2ed72080d855
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-04 13:09:27 +0700

    Update chatbot.py

--- mytools/chatbot.py
+++ mytools/chatbot.py
@@ -4,21 +4,25 @@ import google.generativeai as genai
 class ChatBot:
     def __init__(
         self,
-        name="nor sodikin",
+        name="Tomi",
         apikey="AIzaSyA99Kj3x3lhYCg9y_hAB8LLisoa9Im4PnY",
         intruction=(
-            "Bro, lo ini kayak seorang vibes guru yang paham banget soal tren dan slang kekinian. "
-            "Tugas lo adalah nge-describe apa aja yang lo tangkep dari input-an orang, dengan bahasa "
-            "yang gaul dan bikin orang ketawa. Ga usah serius, yang penting catchy dan seru. "
-            "Deskripsi lo harus singkat, padat, dan jelas. Jangan lebih dari 2000 karakter, dan pastikan "
-            "bahasa lo tetap asik, oke? "
+            "Hei, lo ini kayak temen nongkrong yang paham banget soal semua hal kekinian dan bahasa slang. "
+            "Job desk lo adalah ngejelasin apapun yang lo dapet dari input orang lain, dengan gaya bahasa yang asik dan lucu. "
+            "Ga perlu terlalu serius, yang penting gokil dan ngena. Deskripsinya harus singkat, jelas, dan ga lebih dari 3500 karakter. "
+            "Lo bisa pakai bahasa dari list ini: "
+            "1. Anak gaul "
+            "2. Gaul abis "
+            "3. Kekinian "
+            "4. Santuy "
+            "5. Keren parah "
         ),
     ):
         self.intruction = (
-            intruction + f"Dan nama lu adalah {name}. serta developer, pencipta, dev, pembuat lu adalah: @NorSodikin"
+            intruction + f"Dan nama lo adalah {name}. serta developer, pencipta, dev, pembuat lo adalah: @NorSodikin"
         )
         genai.configure(api_key=apikey)
-        self.model = genai.GenerativeModel("models/gemini-1.5-flash", system_instruction=intruction)
+        self.model = genai.GenerativeModel("models/gemini-1.5-flash", system_instruction=self.intruction)
 
     def Text(self, text):
         try:
