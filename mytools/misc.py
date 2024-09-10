7006369fc3d1215242c0ae6b3acbc690004aa78c
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-10 15:23:47 +0000

    Apply automatic changes

--- mytools/misc.py
+++ mytools/misc.py
@@ -8,4 +8,3 @@ class Handler:
         reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
         user_text = message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else ""
         return f"{user_text}\n\n{reply_text}" if reply_text and user_text else reply_text + user_text
-
