b769b7851b64577b3bbc3b4c6c4359a52f7c68fa
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-10 22:51:23 +0700

    Update misc.py

--- mytools/misc.py
+++ mytools/misc.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+
 class Handler:
     def get_arg(message):
         if message.reply_to_message and len(message.command) < 2:
@@ -8,3 +11,11 @@ class Handler:
         reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
         user_text = message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else ""
         return f"{user_text}\n\n{reply_text}" if reply_text and user_text else reply_text + user_text
+
+    async def send_large_output(message, output):
+        if len(output) <= 4000:
+            await message.reply(output)
+        else:
+            with BytesIO(str.encode(str(output))) as out_file:
+                out_file.name = "result.txt"
+                await message.reply_document(document=out_file)
