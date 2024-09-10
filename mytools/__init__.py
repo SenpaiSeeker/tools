824cd8c4f6a776bdd1eef1681eb06a9c67e4b026
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-10 15:27:39 +0000

    Apply automatic changes

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -1,8 +1,8 @@
 from .button import Button
 from .chatbot import Api
-from .misc import Handler
 from .encrypt import FARNET
 from .getuser import User
+from .misc import Handler
 from .trans import Translate
 
 __version__ = "0.7"
