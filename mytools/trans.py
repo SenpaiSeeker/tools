5b83283df79edf7f95db4b8d82bddc23d6d34364
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-12 03:00:37 +0700

    Update trans.py

--- mytools/trans.py
+++ mytools/trans.py
@@ -1,11 +1,24 @@
+import os
+import string
+import random
+
 from gpytranslate import SyncTranslator
 
 
+def random_name():
+    random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    return random_string + ".mp3"
+
+   
 class Translate(SyncTranslator):
     def ConvertLang(self, msg, lang="id"):
         trans = self.translate(msg, targetlang=lang)
         return trans.text
 
-    def TextToSpeech(self, text, name):
-        with open(name, "wb") as file:
+    def TextToSpeech(self, text):
+        filename = random_name()
+        
+        with open(filename, "wb") as file:
             self.tts(text, file=file)
+            
+        return filename 
