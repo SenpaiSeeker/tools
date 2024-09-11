02bc6e5442d9aaa9636940d5043fc2cde07fcfc9
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-12 02:47:50 +0700

    Update trans.py

--- mytools/trans.py
+++ mytools/trans.py
@@ -2,6 +2,10 @@ from gpytranslate import SyncTranslator
 
 
 class Translate(SyncTranslator):
-    def msg(self, msg, lang="id"):
+    def ConvertLang(self, msg, lang="id"):
         trans = self.translate(msg, targetlang=lang)
         return trans.text
+
+    def TextToSpeech(self, text, name):
+        with open(name, "wb") as file:
+            self.tts(text, file=file)
