d111ee9384af8fd2af53972d72434a8e412772cb
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-11 09:27:38 +0700

    Update misc.py

--- mytools/misc.py
+++ mytools/misc.py
@@ -7,9 +7,9 @@ class Handler:
             return message.reply_to_message.text or message.reply_to_message.caption or ""
         return message.text.split(None, 1)[1] if len(message.command) > 1 else ""
 
-    def get_text(message):
+    def get_text(message, is_chatbot=False):
         reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
-        user_text = message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else ""
+        user_text = message.text if is_chatbot else message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else ""
         return f"{user_text}\n\n{reply_text}" if reply_text and user_text else reply_text + user_text
 
     async def send_large_output(message, output):
