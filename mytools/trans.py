05ea0c0acdf5118c01cae54b81725c688b04dee0
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-11 20:00:53 +0000

    Apply automatic changes

--- mytools/trans.py
+++ mytools/trans.py
@@ -1,15 +1,14 @@
-import os
-import string
 import random
+import string
 
 from gpytranslate import SyncTranslator
 
 
 def random_name():
-    random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
     return random_string + ".mp3"
 
-   
+
 class Translate(SyncTranslator):
     def ConvertLang(self, msg, lang="id"):
         trans = self.translate(msg, targetlang=lang)
@@ -17,8 +16,8 @@ class Translate(SyncTranslator):
 
     def TextToSpeech(self, text):
         filename = random_name()
-        
+
         with open(filename, "wb") as file:
             self.tts(text, file=file)
-            
-        return filename 
+
+        return filename
