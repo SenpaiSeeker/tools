9aef9e1699224c00120a56f8f1075e20f2d51b6d
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-12 21:10:55 +0700

    Update trans.py

--- mytools/trans.py
+++ mytools/trans.py
@@ -5,18 +5,13 @@ import gtts
 from gpytranslate import SyncTranslator
 
 
-def random_name():
-    random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
-    return random_string + ".oog"
-
-
 class Translate(SyncTranslator):
     def ConvertLang(self, msg, lang="id"):
         trans = self.translate(msg, targetlang=lang)
         return trans.text
 
     def TextToSpeech(self, text):
-        filename = random_name()
+        filename = "".join(random.choices(string.ascii_lowercase + string.digits, k=8)) + ".oog"
 
         speech = gtts.gTTS(text, lang="id")
         speech.save(filename)
