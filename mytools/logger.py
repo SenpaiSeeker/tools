d7abde61b90e279472b9bc411c01b16b3cf510c0
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-15 00:20:15 +0700

    Update logger.py

--- mytools/logger.py
+++ mytools/logger.py
@@ -4,26 +4,32 @@ import sys
 
 
 class ColorfulFormatter(logging.Formatter):
+    COLORS = [
+        "\033[1;31m", "\033[1;32m", "\033[1;33m",
+        "\033[1;34m", "\033[1;35m", "\033[1;36m", "\033[1;37m",
+    ]
+
     def format(self, record):
-        color = random.choice(
-            ["\033[1;31m", "\033[1;32m", "\033[1;33m", "\033[1;34m", "\033[1;35m", "\033[1;36m", "\033[1;37m"]
-        )
+        color = random.choice(self.COLORS)
         message = super().format(record)
         return f"{color}{message}\033[0m"
 
 
 class LoggerHandler:
-    def __init__(self, message: str = "[%(levelname)s] - %(name)s - %(message)s - %(asctime)s"):
-        self.formatter = ColorfulFormatter(message)
+    def __init__(self, format_str: str = "[%(levelname)s] - %(name)s - %(message)s - %(asctime)s"):
+        self.formatter = ColorfulFormatter(format_str)
 
-    def logging(self, error_type=False):
+    def setup_logger(self, error_logging: bool = False, log_level=logging.INFO):
         logging.basicConfig(
-            level=logging.INFO,
-            handlers=[logging.StreamHandler(sys.stdout)],
+            level=log_level,
+            handlers=[logging.StreamHandler(sys.stdout)]
         )
         for handler in logging.getLogger().handlers:
             handler.setFormatter(self.formatter)
 
-        if error_type:
+        if error_logging:
             logging.getLogger("pyrogram").setLevel(logging.ERROR)
             logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+
+    def get_logger(name: str):
+        return logging.getLogger(name)
