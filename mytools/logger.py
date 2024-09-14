414229772d9559e2fa197e262ddea3a0226bbf21
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-14 17:05:06 +0000

    Apply automatic changes

--- mytools/logger.py
+++ mytools/logger.py
@@ -1,11 +1,16 @@
-import logging, random, sys
+import logging
+import random
+import sys
 
 
 class ColorfulFormatter(logging.Formatter):
     def format(self, record):
-        color = random.choice(["\033[1;31m", "\033[1;32m", "\033[1;33m", "\033[1;34m", "\033[1;35m", "\033[1;36m", "\033[1;37m"])
+        color = random.choice(
+            ["\033[1;31m", "\033[1;32m", "\033[1;33m", "\033[1;34m", "\033[1;35m", "\033[1;36m", "\033[1;37m"]
+        )
         message = super().format(record)
         return f"{color}{message}\033[0m"
+
 
 class LoggerHandler:
     def __init__(self, message: str = "[%(levelname)s] - %(name)s - %(message)s - %(asctime)s"):
@@ -22,6 +27,3 @@ class LoggerHandler:
         if error_type:
             logging.getLogger("pyrogram").setLevel(logging.ERROR)
             logging.getLogger("asyncio").setLevel(logging.CRITICAL)
-
-
-      
