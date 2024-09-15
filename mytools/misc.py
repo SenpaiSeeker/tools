4906d56510054d3bf15e0c3f22ba1a93b60b10e8
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-15 22:30:01 +0700

    Update misc.py

--- mytools/misc.py
+++ mytools/misc.py
@@ -1,24 +1,21 @@
 from io import BytesIO
 
 
-class Handler:
+class otherMine:
     @staticmethod
-    def get_arg(message):
-        """Mendapatkan argumen dari pesan yang diberikan."""
+    def getArg(message):
         if message.reply_to_message and len(message.command) < 2:
             return message.reply_to_message.text or message.reply_to_message.caption or ""
         return message.text.split(None, 1)[1] if len(message.command) > 1 else ""
 
     @staticmethod
-    def get_text(message, is_chatbot=False):
-        """Mengambil teks dari pesan, termasuk teks balasan jika ada."""
+    def getMsg(message, is_chatbot=False):
         reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
         user_text = message.text if is_chatbot else (message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else "")
         return f"{user_text}\n\n{reply_text}".strip() if reply_text and user_text else reply_text + user_text
 
     @staticmethod
-    async def send_large_output(message, output):
-        """Mengirimkan output, jika terlalu panjang dikirim sebagai file."""
+    async def sendLongPres(message, output):
         if len(output) <= 4000:
             await message.reply(output)
         else:
@@ -27,9 +24,8 @@ class Handler:
                 await message.reply_document(document=out_file)
 
     @staticmethod
-    async def get_time(seconds):
-        """Mengkonversi waktu dari detik ke format yang lebih mudah dibaca."""
-        time_units = [(60, "s"), (60, "m"), (24, "h"), (7, "d"), (4.34812, "w")]  # Detik  # Menit  # Jam  # Hari  # Minggu
+    async def getTime(seconds):
+        time_units = [(60, "s"), (60, "m"), (24, "h"), (7, "d"), (4.34812, "w")]
         result = []
 
         for unit_seconds, suffix in time_units:
