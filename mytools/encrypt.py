fe27072a14f45f24fd2d144d852ab6a3f29ccc4f
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-16 05:31:11 +0000

    Apply automatic changes

--- mytools/encrypt.py
+++ mytools/encrypt.py
@@ -56,11 +56,11 @@ class FARNET:
 
 class BinaryEncryptor:
     def text_to_binary(self, text):
-        return ''.join(format(ord(char), '08b') for char in text)
+        return "".join(format(ord(char), "08b") for char in text)
 
     def binary_to_text(self, binary):
-        chars = [binary[i:i+8] for i in range(0, len(binary), 8)]
-        return ''.join(chr(int(char, 2)) for char in chars)
+        chars = [binary[i : i + 8] for i in range(0, len(binary), 8)]
+        return "".join(chr(int(char, 2)) for char in chars)
 
     def encrypt(self, text):
         return self.text_to_binary(text)
