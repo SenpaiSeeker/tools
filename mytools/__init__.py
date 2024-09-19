1340ae43374eee6d2ea6109c6aca8fb9e3c23224
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-09-19 10:57:29 +0000

    Apply automatic changes

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -1,9 +1,9 @@
 from .button import Button
 from .chatbot import Api, ImageGen
+from .database import DataBase
 from .encrypt import FARNET, BinaryEncryptor
 from .getuser import Extract
 from .logger import LoggerHandler
-from .database import DataBase
 from .misc import Handler
 from .trans import Translate
 
