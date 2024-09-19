e815868950f2ad48d68c5e15eac918359cb18478
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-19 17:57:10 +0700

    Update __init__.py

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -3,6 +3,7 @@ from .chatbot import Api, ImageGen
 from .encrypt import FARNET, BinaryEncryptor
 from .getuser import Extract
 from .logger import LoggerHandler
+from .database import DataBase
 from .misc import Handler
 from .trans import Translate
 
