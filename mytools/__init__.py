7c4a5654e46bb6509e2399bf575d6737a8d501a7
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-21 21:21:30 +0700

    Update __init__.py

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -7,7 +7,7 @@ from .logger import LoggerHandler
 from .misc import Handler
 from .trans import Translate
 
-__version__ = "0.0.0.1.dev10"
+__version__ = "0.0.0.1.dev11"
 
 mytoolsID = """
  __    __     __  __     ______   ______     ______     __         ______     __     _____    
