b2b8841445cc4064553fe8808287a022427cb082
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-09-26 00:51:21 +0700

    Update misc.py

--- mytools/misc.py
+++ mytools/misc.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import base64
 
 
 class Handler:
@@ -11,14 +12,6 @@ class Handler:
         reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
         user_text = message.text if is_chatbot else (message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else "")
         return f"{user_text}\n\n{reply_text}".strip() if reply_text and user_text else reply_text + user_text
-
-    async def sendLongPres(self, message, output):
-        if len(output) <= 4000:
-            await message.reply(output)
-        else:
-            with BytesIO(output.encode()) as out_file:
-                out_file.name = "result.txt"
-                await message.reply_document(document=out_file)
 
     def getTime(self, seconds):
         time_units = [(60, "s"), (60, "m"), (24, "h"), (7, "d"), (4.34812, "w")]
@@ -35,3 +28,25 @@ class Handler:
             return "0s"
 
         return ":".join(result[::-1])
+
+    async def sendLongPres(self, message, output):
+        if len(output) <= 4000:
+            await message.reply(output)
+        else:
+            with BytesIO(output.encode()) as out_file:
+                out_file.name = "result.txt"
+                await message.reply_document(document=out_file)
+
+    async def encode(self, string: str):
+        string_bytes = string.encode("ascii")
+        base64_bytes = base64.urlsafe_b64encode(string_bytes)
+        
+        return base64_bytes.decode("ascii").rstrip("=")
+
+    async def decode(self, base64_string: str):
+        base64_string = base64_string.rstrip("=")
+        padding_needed = "=" * (-len(base64_string) % 4)
+        base64_bytes = (base64_string + padding_needed).encode("ascii")
+        
+        string_bytes = base64.urlsafe_b64decode(base64_bytes)
+        return string_bytes.decode("ascii")
