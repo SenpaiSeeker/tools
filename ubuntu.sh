250b0a92b884369ec700b4102573eb58821b523c
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-01-16 11:29:01 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,7 +7,7 @@ proot-distro login ubuntu
 apt update && apt upgrade -y
 apt install curl wget software-properties-common -y
 
-apt install python3 python3-pip -y
+apt install python3 python3-pip python3-venv -y
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt install nodejs -y
