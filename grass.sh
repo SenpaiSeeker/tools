bc97a7f9a5930ec7deb9f27b79a5456f555e3555
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-02-24 22:03:40 +0700

    Update grass.sh

--- grass.sh
+++ grass.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/SenpaiSeeker/getgrass
 cd getgrass
 
-python3 -m venv env && source env/bin/act* pip3 install -r requirements.txt
+python3 -m venv env && source env/bin/act* && pip3 install -r requirements.txt
 
 echo "$1" > token.txt
 
