b8e5b1b2d8bcb8a883a622a991e7d98984b5678f
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-02-24 21:47:19 +0700

    Create grass.sh

---
+++ grass.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/SenpaiSeeker/getgrass
+cd getgrass
+
+python3 -m venv env && source env/bin/act* pip3 install -r requirements.txt
+
+echo "$1" > token.txt
+
+bash start.sh 
