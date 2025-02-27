1da0ec7f9a2a6e8fdb5559865c4dc05d14078e8c
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-02-27 11:52:37 +0700

    Update teneo.sh

--- teneo.sh
+++ teneo.sh
@@ -3,6 +3,6 @@ cd Teneo-BOT
 
 python3 -m venv env && source env/bin/activate && pip3 install -r requirements.txt 
 
-> tokens.txt && echo "$1" > tokens.txt
+echo "$1" > tokens.txt
 
 echo -e "3" | python3 bot.py
