03c4e319c8d0d5dc3aa0310f06daa0e1856122a5
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-02-28 23:26:58 +0700

    Create litas.sh

---
+++ litas.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/MeoMunDep/LITAS
+cd LITAS
+
+npm install --force user-agents axios colors p-limit https-proxy-agent socks-proxy-agent crypto-js ws uuid xlsx readline-sync moment lodash qs
+
+echo "$1" > datas.txt
+
+node meomundep.js
