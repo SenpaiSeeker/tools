2513a8c7fd89397288c061127ee164cf29b6b7b4
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-02-28 23:30:29 +0700

    Update litas.sh

--- litas.sh
+++ litas.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/MeoMunDep/LITAS
-cd LITAS
+cd LITAS/LITAS
 
 npm install --force user-agents axios colors p-limit https-proxy-agent socks-proxy-agent crypto-js ws uuid xlsx readline-sync moment lodash qs
 
