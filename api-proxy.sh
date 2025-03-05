cb28bd0b36beabc0e010363d5c0470ee185d2d4b
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-03-05 14:41:15 +0700

    Update api-proxy.sh

--- api-proxy.sh
+++ api-proxy.sh
@@ -1,6 +1,13 @@
 PROXY_URLS="https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt"
 PROXY_FILE="${1:-proxy.txt}"
 
+function generate_random_color() {
+    local r=$(shuf -i 128-255 -n 1)
+    local g=$(shuf -i 128-255 -n 1)
+    local b=$(shuf -i 128-255 -n 1)
+    echo "\033[38;2;${r};${g};${b}m"
+}
+
 function log_message() {
     local type="$1"
     local message="$2"
@@ -14,7 +21,7 @@ function log_message() {
         *) color="\033[1;37m";;
     esac
 
-    echo -e "\033[1;37m[$datetime] \033[1;35m| ${color}[$type] \033[1;35m| \033[1;34m$message\033[0m"
+    echo -e "\033[1;37m[$datetime] \033[1;35m| ${color}[$type] \033[1;35m| $(generate_random_color)$message\033[0m"
 }
 
 function fetch_proxies() {
@@ -40,8 +47,9 @@ function process_proxies() {
     > "$PROXY_FILE"
 
     fetch_proxies "$PROXY_URLS"
-
-    local total_count=$(wc -l < "$PROXY_FILE")
+    
+    local total_count
+    total_count=$(wc -l < "$PROXY_FILE")
     log_message "INFO" "Total proxies saved to $PROXY_FILE: $total_count."
 }
 
