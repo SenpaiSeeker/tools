23e58b126ba9ce579803ebaa15da2cc978f2ae64
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-03-07 23:18:19 +0700

    Delete grass.sh

--- grass.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/SenpaiSeeker/getgrass
-cd getgrass
-
-python3 -m venv env && source env/bin/act* && pip3 install -r requirements.txt
-
-echo "$1" > token.txt
-
-bash start.sh 
