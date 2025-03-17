10c970947832ff61522638f24abfc281c398cfd7
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-03-17 11:13:54 +0700

    Update sparkchain.sh

--- sparkchain.sh
+++ sparkchain.sh
@@ -5,7 +5,4 @@ echo '[{"Email": "'"$1"'", "Password": "'"$2"'"}]' > accounts.json
 
 python3 -m venv env && source env/bin/activate && pip3 install -r requirements.txt 
 
-curl -sL https://raw.githubusercontent.com/SenpaiSeeker/tools/refs/heads/main/api-proxy.sh | bash -s proxy.txt 
-
-total_count=$(wc -l < proxy.txt)
-echo -e "2\n$total_count" | python3 bot.py
+echo -e "3" | python3 bot.py
