9e50ffb8f39e417eb9e751dbefc123db364504d6
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-03-18 16:39:34 +0700

    Update api-proxy.sh

--- api-proxy.sh
+++ api-proxy.sh
@@ -1,6 +1,5 @@
 PROXY_URLS="https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt"
 PROXY_FILE="${1:-proxy.txt}"
-TEST_URL="http://httpbin.org/ip"
 
 function generate_random_color() {
     local r=$(shuf -i 128-255 -n 1)
@@ -32,39 +31,23 @@ function fetch_proxies() {
     curl -s "$url" -o "$temp_file"
     if [[ $? -eq 0 ]]; then
         log_message "INFO" "Fetched proxies successfully."
+        cat "$temp_file" > "$PROXY_FILE"
     else
         log_message "ERROR" "Failed to fetch proxies from $url."
         rm -f "$temp_file"
         exit 1
     fi
-}
-
-function check_proxy() {
-    local proxy="$1"
-    if curl -s --proxy "$proxy" --max-time 5 "$TEST_URL" > /dev/null; then
-        echo "$proxy" >> "$PROXY_FILE"
-        log_message "INFO" "Valid proxy: $proxy"
-    else
-        log_message "ERROR" "Invalid proxy: $proxy"
-    fi
+    rm -f "$temp_file"
 }
 
 function process_proxies() {
     clear
-    log_message "INFO" "Starting to fetch and verify proxies..."
-    > "$PROXY_FILE"
+    log_message "INFO" "Starting to fetch proxies..."
 
     fetch_proxies "$PROXY_URLS"
 
-    while IFS= read -r proxy; do
-        check_proxy "$proxy" &
-    done < "temp_proxies.txt"
-
-    wait
-    rm -f "temp_proxies.txt"
-
     local total_count=$(wc -l < "$PROXY_FILE")
-    log_message "INFO" "Total valid proxies saved to $PROXY_FILE: $total_count."
+    log_message "INFO" "Total proxies saved to $PROXY_FILE: $total_count."
 }
 
 process_proxies
